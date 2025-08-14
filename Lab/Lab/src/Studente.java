e01fbfcfac91ac7ddb69cf1decb6bf9e577e507e
mirkom117 <165894301+mirkom117@users.noreply.github.com>
2025-08-14 09:35:41 +0200

    Create class Studente.java

---
+++ Lab/Lab/src/Studente.java
@@ -0,0 +1,25 @@
+public class Studente {
+    private String nome;
+    private String cognome;
+
+    public Studente(String nome, String cognome) {
+        this.nome = nome;
+        this.cognome = cognome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+}
