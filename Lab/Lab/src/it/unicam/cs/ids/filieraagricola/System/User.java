707165530e7521439e7f95ae92ebe07752a2235c
mirkom117 <165894301+mirkom117@users.noreply.github.com>
2025-08-26 17:23:11 +0200

    Update User.java

--- Lab/Lab/src/it/unicam/cs/ids/filieraagricola/System/User.java
+++ Lab/Lab/src/it/unicam/cs/ids/filieraagricola/System/User.java
@@ -1,10 +1,19 @@
-package it.unicam.cs.ids.filieraagricola.System;
+package it.unicam.cs.ids.filieraagricola.system;
 
 public class User {
     private int id;
     private String name;
     private String password;
     private String email;
+    private String[]  permissions;
+
+    public String[] getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(String[] permissions) {
+        this.permissions = permissions;
+    }
 
     public User(int id, String name, String password, String email) {
         this.id = id;
