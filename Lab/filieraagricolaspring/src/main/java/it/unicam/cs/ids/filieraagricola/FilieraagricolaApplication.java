b0b6f1b27196984e6c3e0351c426c3dea3677987
) <62116880+menestresser@users.noreply.github.com>
2025-08-29 13:22:50 +0200

    Adding spring boot project

---
+++ Lab/filieraagricolaspring/src/main/java/it/unicam/cs/ids/filieraagricola/FilieraagricolaApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.filieraagricola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilieraagricolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilieraagricolaApplication.class, args);
+	}
+
+}
