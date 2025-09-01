cf792a90a50236f7ba27830912420eccdd26f08f
Ennio Lizza <enni.lizza@gmail.com>
2025-09-01 16:48:34 +0200

    Create enum ActorType.java

---
+++ Lab/Lab/src/it/unicam/cs/ids/filieraagricola/model/ActorType.java
@@ -0,0 +1,14 @@
+package it.unicam.cs.ids.filieraagricola.model;
+
+/**
+ * Represents the different types of actors in the agricultural supply chain platform.
+ */
+public enum ActorType {
+    PRODUCER,
+    TRANSFORMER,
+    DISTRIBUTOR,
+    CURATOR,
+    ANIMATOR,
+    BUYER,
+    PLATFORM_MANAGER
+}
