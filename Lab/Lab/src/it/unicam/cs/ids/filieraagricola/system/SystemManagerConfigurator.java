eefff0282698f0d8e723f2541f5cf6fcfe0fb455
mirkom117 <165894301+mirkom117@users.noreply.github.com>
2025-09-03 15:25:56 +0200

    create action for user roles

--- Lab/Lab/src/it/unicam/cs/ids/filieraagricola/system/SystemManagerConfigurator.java
+++ Lab/Lab/src/it/unicam/cs/ids/filieraagricola/system/SystemManagerConfigurator.java
@@ -1,19 +1,16 @@
 package it.unicam.cs.ids.filieraagricola.system;
 
-import it.unicam.cs.ids.filieraagricola.service.UserService;
-
 public class SystemManagerConfigurator {
     public static void configure() {
         SystemManager system = SystemManager.getInstance();
+
         system.addMap("login", new LoginAction());
         system.addMap("anonymousLogin", new AnonymousLoginAction());
-        system.addMap("approve", new AuthorizeAction(new ApproveContentAction(), "approve rights"));
-        system.addMap("create event", new CreateEventAction());
-        system.addMap("create event", new AuthorizeAction(new CreateEventAction(), "events rights"));
-
-
-        // Registering user's prototype
-        system.getUserService().registerPrototype("Approver", UserService.makePrototype("approve rights"));
+        system.addMap("approve_content", new AuthorizeAction(new ApproveContentAction(), "content_curate"));
+        system.addMap("create_event", new AuthorizeAction(new CreateEventAction(), "events_manage"));
+        system.addMap("create_product", new AuthorizeAction(new CreateProductAction(), "products_manage"));
+        system.addMap("register_event", new AuthorizeAction(new RegisterForEventAction(), "events_participate"));
+        system.addMap("distributor_create_package", new AuthorizeAction(new DistributorCreatePackageAction(), "distributor_rights"));
 
     }
 }
